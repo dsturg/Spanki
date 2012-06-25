17b0808cdf5b98b53b2c0acff862b6fd8cd5f263
Dave Sturgill <dave.sturgill@gmail.com>
2012-06-25 17:53:15 -0400

    Minor splicecomp typo, error reporting for missing files

--- analysis_commands.sh
+++ analysis_commands.sh
@@ -6,8 +6,8 @@ spankijunc -m all -o male_rep1 -i testdata/male_r1.bam -g testdata/annotation/ge
 spankijunc -m all -o male_rep2 -i testdata/male_r2.bam -g testdata/annotation/genemodels.gtf -f testdata/fasta/myref.fa
 
 ECHO "[***************] Merging the replicates together"
-merge_jtabs_all female_rep1/juncs.all,female_rep2/juncs.all > female_repsmerged.juncs
-merge_jtabs_all male_rep1/juncs.all,male_rep2/juncs.all > male_repsmerged.juncs
+merge_jtabs female_rep1/juncs.all,female_rep2/juncs.all > female_repsmerged.juncs
+merge_jtabs male_rep1/juncs.all,male_rep2/juncs.all > male_repsmerged.juncs
 
 ECHO "[***************] Parsing asta output"
 spankisplice -o female_repsmerged_asta -j female_repsmerged.juncs -c testdata/female_cuff/isoforms.fpkm_tracking -f testdata/fasta/myref.fa -g testdata/annotation/genemodels.gtf -a testdata/annotation/genemodels_splices.out 
