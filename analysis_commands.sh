d6c0d3aef16057b653303f370f5317060be7c655
Dave Sturgill <dave.sturgill@gmail.com>
2013-07-22 16:27:06 -0400

    Some edits to example code

--- analysis_commands.sh
+++ analysis_commands.sh
@@ -3,10 +3,10 @@
 # Uses testdata availble from:
 # http://www.cbcb.umd.edu/software/spanki/testdata.tar.gz
 
-ECHO "[***************] Starting female spankijunc runs"
+echo "[***************] Starting female spankijunc runs"
 spankijunc -m all -o female_rep1 -i testdata/female_r1.bam -g testdata/annotation/genemodels.gtf -f testdata/fasta/myref.fa
 spankijunc -m all -o female_rep2 -i testdata/female_r2.bam -g testdata/annotation/genemodels.gtf -f testdata/fasta/myref.fa
-ECHO "[***************] Starting male spankijunc runs"
+echo "[***************] Starting male spankijunc runs"
 spankijunc -m all -o male_rep1 -i testdata/male_r1.bam -g testdata/annotation/genemodels.gtf -f testdata/fasta/myref.fa
 spankijunc -m all -o male_rep2 -i testdata/male_r2.bam -g testdata/annotation/genemodels.gtf -f testdata/fasta/myref.fa
 
@@ -14,19 +14,19 @@ spankijunc -m all -o male_rep2 -i testdata/male_r2.bam -g testdata/annotation/ge
 # then create a list of junctions (either curated, or a merge of all junctions detected), and run make_curated_jtab
 # on each sample. Example:
 
-ECHO "[***************] Making a table of all junctions detected in any sample"
-cat female_rep1/juncs.all female_rep2/juncs.all female_rep1/juncs.all female_rep2/juncs.all > all_tables.txt
+echo "[***************] Making a table of all junctions detected in any sample"
+cat female_rep1/juncs.all female_rep2/juncs.all male_rep1/juncs.all male_rep2/juncs.all > all_tables.txt
 head -1 female_rep1/juncs.all | awk '{print $1"\t"$4"\t"$7"\t"$8"\t"$9"\t"$14"\t"$15}' > all_juncs.txt 
 awk '{if ($1 != "juncid") {print $1"\t"$4"\t"$7"\t"$8"\t"$9"\t"$14"\t"$15}}' all_tables.txt | sort -u >> all_juncs.txt
 
-ECHO "[***************] Getting additional data from the female runs"
+echo "[***************] Getting additional data from the female runs"
 make_curated_jtab -o female_rep1_curated -i  testdata/female_r1.bam -jlist all_juncs.txt -jtab female_rep1/juncs.all
 make_curated_jtab -o female_rep2_curated -i  testdata/female_r2.bam -jlist all_juncs.txt -jtab female_rep2/juncs.all
-ECHO "[***************] Getting additional data from the male runs"
+echo "[***************] Getting additional data from the male runs"
 make_curated_jtab -o male_rep1_curated -i  testdata/male_r1.bam -jlist all_juncs.txt -jtab male_rep1/juncs.all
 make_curated_jtab -o male_rep2_curated -i  testdata/male_r2.bam -jlist all_juncs.txt -jtab male_rep2/juncs.all
 
-ECHO "[***************] Merging the replicates together"
+echo "[***************] Merging the replicates together"
 merge_jtabs female_rep1_curated/juncs.all,female_rep2_curated/juncs.all > female_repsmerged.juncs
 merge_jtabs male_rep1_curated/juncs.all,male_rep2_curated/juncs.all > male_repsmerged.juncs
 
@@ -34,11 +34,11 @@ merge_jtabs male_rep1_curated/juncs.all,male_rep2_curated/juncs.all > male_repsm
 #merge_jtabs female_rep1/juncs.all,female_rep2/juncs.all > female_repsmerged.juncs
 #merge_jtabs male_rep1/juncs.all,male_rep2/juncs.all > male_repsmerged.juncs
 
-ECHO "[***************] Parsing events output"
+echo "[***************] Parsing events output"
 spankisplice -o female_repsmerged_events -j female_repsmerged.juncs -c testdata/female_cuff/isoforms.fpkm_tracking -f testdata/fasta/myref.fa -g testdata/annotation/genemodels.gtf -a testdata/annotation/genemodels_splices.out 
 spankisplice -o male_repsmerged_events -j male_repsmerged.juncs -c testdata/male_cuff/isoforms.fpkm_tracking -f testdata/fasta/myref.fa -g testdata/annotation/genemodels.gtf -a testdata/annotation/genemodels_splices.out 
 
-ECHO "[***************] Doing pairwise comparison"
+echo "[***************] Doing pairwise comparison"
 splicecomp -a female_repsmerged_events/events.out -b male_repsmerged_events/events.out -o F_vs_M_splicecomp
 junccomp -a female_repsmerged.juncs -b male_repsmerged.juncs -o F_vs_M_junccomp
 
@@ -47,11 +47,11 @@ junccomp -a female_repsmerged.juncs -b male_repsmerged.juncs -o F_vs_M_junccomp
 ########################
 
 # Annotating a junction set (From a junction table)
-annotate_junctions -o female_rep1_annotated_junctab -jtab female_rep1/juncs.all -f ~/data/indexes/dm3_NISTnoUextra.fa -g ~/data/annotation/dmel_BDGP5.39.67_ed.gtf
+annotate_junctions -o female_rep1_annotated_junctab -jtab female_rep1/juncs.all -f testdata/fasta/myref.fa -g testdata/annotation/genemodels.gtf
 # Annotating a junction set (From a junction list)
-annotate_junctions -o female_rep1_annotated_junclist -jlist female_rep1/juncs.list -f ~/data/indexes/dm3_NISTnoUextra.fa -g ~/data/annotation/dmel_BDGP5.39.67_ed.gtf
+annotate_junctions -o female_rep1_annotated_junclist -jlist female_rep1/juncs.list -f testdata/fasta/myref.fa -g testdata/annotation/genemodels.gtf
 # Annotating annotated junctions
-annotate_junctions -o annotated_reference_juncs -f ~/data/indexes/dm3_NISTnoUextra.fa -g ~/data/annotation/dmel_BDGP5.39.67_ed.gtf
+annotate_junctions -o annotated_reference_juncs -f testdata/fasta/myref.fa -g testdata/annotation/genemodels.gtf
 
 
 
--- plotting_code.R
+++ plotting_code.R
@@ -12,6 +12,11 @@
 
 
 #  Load the library
+#  Make sure you have "vcd" installed
+#  See:
+#  http://cran.r-project.org/web/packages/vcd/index.html
+#  David Meyer, Achim Zeileis, and Kurt Hornik (2012). vcd: Visualizing Categorical Data. R package version 1.2-13.
+
 library(vcd)
 
 
@@ -26,7 +31,7 @@ library(vcd)
 
 
 #///////////////////
-Example data
+# Basic fourfold and mosaic plots
 #//////////////////
 
 # Consider a splicing event with the following data:
@@ -49,17 +54,32 @@ fourfoldplot(mymat)
 # Make a mosaic plot
 mosaic(mymat)
 
-#\\\\\\\\\\\\\\\\\\
-Variations
-#\\\\\\\\\\\\\\\\\\\
-
-
 # Alternative fill colors for mosaics:
 
 fill_colors <- matrix(c("dark cyan", "gray", "gray", "dark magenta"), ncol = 2)
-fill_colors <- matrix(c("blue", "blue", "red", "red"), ncol = 2)
-
 mosaic(mymat, gp = gpar(fill = fill_colors, col = 0))
+fill_colors <- matrix(c("blue", "blue", "red", "red"), ncol = 2)
+mosaic(mymat, gp = gpar(fill = fill_colors, col = 0))
+
+
+#///////////////////
+# Example mosaic plot 
+# using sample data output
+#//////////////////
+
+# Set the directory to 
+setwd("~/Desktop/spankitest/")
+
+splicecomp <- read.delim(file="F_vs_M_splicecomp/event_compare.out",stringsAsFactors=F)
+
+i <- 1
+
+mymat <- matrix(c(splicecomp$inc1[i],splicecomp$exc1[i],splicecomp$inc2[i],splicecomp$exc2[i]),ncol=2,byrow=T)
+
+# Make a mosaic plot
+fill_colors <- matrix(c("blue", "blue", "red", "red"), ncol = 2)
+mosaic(mymat, gp = gpar(fill = fill_colors, col = 0)
+
 
 
 
